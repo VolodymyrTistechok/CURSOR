31b8cd893f86b845e041435a90d71d29000f72bd
VolodymyrTistechok <81178483+VolodymyrTistechok@users.noreply.github.com>
2021-04-26 19:06:25 +0300

    HW GC

---
+++ HW10/src/Test.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<Integer[]> m = new ArrayList<>();
+        while (true) {
+            m.add(new Integer[2147483647]);
+        }
+    }
+}
