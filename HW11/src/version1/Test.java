7ab42930bab2196d936a0465bdbf3dad9d0811c5
VolodymyrTistechok <81178483+VolodymyrTistechok@users.noreply.github.com>
2021-05-10 11:37:19 +0300

    one method

--- HW11/src/version1/Test.java
+++ HW11/src/version1/Test.java
@@ -2,6 +2,7 @@ package version1;
 
 public class Test {
     public static void main(String[] args) {
+        //one method
        new PingPong("Ping", "ping").start();
        new PingPong("Pong","pong").start();
     }
